8bac75e04a70be15250b42188f2235292639f416
Abdulazizi2008 <ozodbekovabdu@gmail.com>
2024-10-01 17:00:41 +0500

    init

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,4 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import productsSlice from "./productsSlice";
 import colorSlice from "./colorSlice";
 import brandSlice from "./brandSlice";
